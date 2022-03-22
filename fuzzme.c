522186f76a06a3d0e82cef87aa56bb0cc008b26f
Nathan Jackson <nathan@forallsecure.com>
2022-03-22 15:16:31 -0400

    initial commit

---
+++ fuzzme.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int fuzzme(char *buf)
+{
+  if(strlen(buf) >= 3)
+    if(buf[0] == 'b')
+      if(buf[1] == 'u')
+        if(buf[2] == 'g') {
+          printf("You've got it!");
+          abort();
+        }
+    return 0;
+}
+
+
+int LLVMFuzzerTestOneInput(char* data, size_t size)
+{
+    fuzzme(data);
+    return 0;
+}
+
