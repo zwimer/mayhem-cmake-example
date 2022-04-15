80530144e09e1964029b649d3851394129dcab49
zwimer <zwimer@gmail.com>
2022-04-15 11:46:31 -0700

    add GitHub action to launch Mayhem

--- fuzzme.c
+++ fuzzme.c
@@ -17,28 +17,34 @@ int fuzzme(char *buf)
 
 #define BUFSZ 256
 
-int main(int argc, char** argv)
-{
-    char buf[BUFSZ] = { 0 };
-    FILE* f = NULL;
-    size_t nr = 0;
 
-    if (2 > argc) {
-        fprintf(stderr, "usage: %s PAYLOAD\n", argv[0]);
-        return 1;
-    }
-
-    f = fopen(argv[1], "rb");
-    assert(f);
-
-    nr = fread(buf, sizeof(buf[0]), BUFSZ, f);
-    assert(nr > 0);
-    buf[BUFSZ-1] = '\0';
-
-    fuzzme(buf);
-
-    fclose(f);
-
-    return 0;
+int LLVMFuzzerTestOneInput(char* data, size_t size) {
+	(void) size;
+	return fuzzme(data);
 }
+
+/* int main(int argc, char** argv) */
+/* { */
+/*     char buf[BUFSZ] = { 0 }; */
+/*     FILE* f = NULL; */
+/*     size_t nr = 0; */
+
+/*     if (2 > argc) { */
+/*         fprintf(stderr, "usage: %s PAYLOAD\n", argv[0]); */
+/*         return 1; */
+/*     } */
+
+/*     f = fopen(argv[1], "rb"); */
+/*     assert(f); */
+
+/*     nr = fread(buf, sizeof(buf[0]), BUFSZ, f); */
+/*     assert(nr > 0); */
+/*     buf[BUFSZ-1] = '\0'; */
+
+/*     fuzzme(buf); */
+
+/*     fclose(f); */
+
+/*     return 0; */
+/* } */
 
